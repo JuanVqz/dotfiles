6310a657f4cfeab2280703cc4e1e65d3717a9085
Juan Vasquez <juan@logicalbricks.com>
2020-04-06 23:23:32 -0500

    Add brew scripting
    When I reinstall macOs system, I spend a lot of time installing
    apps with this that is yesterday think :D

---
+++ scripts/brew.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+brew cask install firefox
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install iterm2
+brew cask install obs
+brew cask install calibre
+
+brew install neovim
+brew install tmux
