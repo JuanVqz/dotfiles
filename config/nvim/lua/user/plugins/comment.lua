8e8b244f22a776cf6d6f8497abdec704a46a9b9b
Juan Vasquez <juan@ombulabs.com>
2022-10-26 22:52:01 -0500

    Add Comment documentation

--- config/nvim/lua/user/plugins/comment.lua
+++ config/nvim/lua/user/plugins/comment.lua
@@ -1,3 +1,10 @@
+-- Useful mappings
+-- 'gcc', toggle comment current line
+-- 'gbc', toggle comment current block
+-- 'gcO', Add comment on the line above
+-- 'gco', Add comment on the line below
+-- 'gcA', Add comment at the end of line
+
 local ok, comment = pcall(require, "Comment")
 if not ok then
   return
