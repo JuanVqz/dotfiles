d1d910e7e15e426b0d46ba86374aec639ddb9304
Juan Vasquez <juan@ombulabs.com>
2023-02-26 21:51:58 -0600

    Add ToggleBackground command

---
+++ config/nvim/lua/user/commands.lua
@@ -0,0 +1,9 @@
+local create_cmd = vim.api.nvim_create_user_command
+
+create_cmd("ToggleBackground", function ()
+    if vim.o.background == 'dark' then
+        vim.cmd'set bg=light'
+    else
+        vim.cmd'set bg=dark'
+    end
+end, {})
