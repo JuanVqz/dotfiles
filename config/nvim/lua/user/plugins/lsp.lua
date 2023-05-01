b1693195b2949a3d13c2e31d2d0c6ccd74b6d9aa
Juan Vasquez <juan@ombulabs.com>
2023-05-01 15:25:51 -0600

    Load friendly-snippets
    Reference https://github.com/rafamadriz/friendly-snippets/issues/262

--- config/nvim/lua/user/plugins/lsp.lua
+++ config/nvim/lua/user/plugins/lsp.lua
@@ -1,6 +1,9 @@
 local ok, lsp = pcall(require, "lsp-zero")
 if not ok then return end
 
+require("luasnip.loaders.from_vscode").load()
+require("luasnip").filetype_extend("ruby", {"rails"})
+
 lsp.preset("recommended")
 
 lsp.nvim_workspace()
--- config/nvim/lua/user/plugins/luasnip.lua
+++
@@ -1,4 +0,0 @@
-local ok, ls = pcall(require, "luasnip")
-if not ok then return end
-
-ls.filetype_extend("ruby", {"rails"})
