87efa6767e7caaff19596a8a783426fbd5689f3a
Juan Vasquez <juan@ombulabs.com>
2023-07-04 19:19:36 -0600

    Remove treesitter playground config

--- config/nvim/lua/user/plugins/nvim-treesitter.lua
+++ config/nvim/lua/user/plugins/nvim-treesitter.lua
@@ -20,10 +20,4 @@ configs.setup({
   indent = {
     enable = true,
   },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-  },
 })
