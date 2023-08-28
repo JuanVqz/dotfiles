25acb97b5d0dae0397e68088f3ee31aa48e103c3
Juan Vasquez <juan@ombulabs.com>
2023-08-27 23:29:23 -0600

    Comment git keybindings

--- config/nvim/lua/user/plugins/vim-fugitive.lua
+++ config/nvim/lua/user/plugins/vim-fugitive.lua
@@ -3,13 +3,12 @@ local keymap = vim.api.nvim_set_keymap
 
 local opts = { noremap = true, silent = true }
 
-keymap("n", "<leader>gs", ":Git<CR>", opts)
-keymap("n", "<leader>gl", ":G pull<CR>", opts)
-keymap("n", "<leader>gp", ":G push<CR>", opts)
-keymap("n", "<leader>gf", ":G push --force-with-lease<CR>", opts)
-keymap("n", "<leader>gw", ":Gwrite<CR>", opts)
-keymap("n", "<leader>gr", ":Gread<CR>", opts)
-keymap("n", "<leader>gb", ":Git blame<CR>", opts)
-keymap("v", "<leader>gb", ":GBrowse!<CR>", opts)
+-- keymap("n", "<leader>gs", ":Git<CR>", opts)
+-- keymap("n", "<leader>gl", ":G pull<CR>", opts)
+-- keymap("n", "<leader>gp", ":G push<CR>", opts)
+-- keymap("n", "<leader>gf", ":G push --force-with-lease<CR>", opts)
+-- keymap("n", "<leader>gw", ":Gwrite<CR>", opts)
+-- keymap("n", "<leader>gr", ":Gread<CR>", opts)
+-- keymap("n", "<leader>gb", ":Git blame<CR>", opts)
+-- keymap("v", "<leader>gb", ":GBrowse!<CR>", opts)
 -- keymap("n", "<leader>gv", ":Gvdiffsplit!<CR>", opts)
-
