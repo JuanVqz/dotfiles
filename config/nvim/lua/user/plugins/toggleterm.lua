d7c1a53feb16affdd2d2da2e58e53b6fa804b0b4
Juan Vasquez <juan@ombulabs.com>
2023-09-07 00:43:48 -0600

    Add lazygit to nvim Yey!!

--- config/nvim/lua/user/plugins/toggleterm.lua
+++ config/nvim/lua/user/plugins/toggleterm.lua
@@ -5,3 +5,12 @@ toggleterm.setup({
   open_mapping = "<c-t>",
   direction = "float",
 })
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", dir = "git_dir", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>hl", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
