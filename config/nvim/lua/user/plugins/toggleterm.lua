d2a92ce64ada0f40439528e2acae050a43cc558f
Juan Vasquez <juan@ombulabs.com>
2023-09-08 17:00:11 -0600

    Improve which-key + toggle_terminal config
    I was playing around lazygit bc want to test it when found this video
    https://www.youtube.com/watch?v=Qtdbco50sPc&t=538s
    It shows me how to improve my which-key config, he recommended the
    following way to toggle a terminal and lazygit but I actually made it
    globally because that way don't loose terminal status
    
    ```lua
    local toggle_float = function()
      local float = Terminal:new({ direction = "float", hidden = true })
      float:toggle()
    end
    
    local toggle_lazygit = function()
      local lazygit = Terminal:new({ direction = "float", cmd = "lazygit", dir = "git_dir", hidden = true })
      lazygit:toggle()
    end
    ```

--- config/nvim/lua/user/plugins/toggleterm.lua
+++ config/nvim/lua/user/plugins/toggleterm.lua
@@ -1,16 +1,17 @@
 local ok, toggleterm = pcall(require, "toggleterm")
 if not ok then return end
 
-toggleterm.setup({
-  open_mapping = "<c-t>",
-  direction = "float",
-})
+toggleterm.setup()
+-- toggleterm.setup({
+--   open_mapping = "<c-t>",
+--   direction = "float",
+-- })
 
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", dir = "git_dir", hidden = true })
-
-function _lazygit_toggle()
-  lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap("n", "<leader>hl", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+-- local Terminal  = require('toggleterm.terminal').Terminal
+-- local lazygit = Terminal:new({ cmd = "lazygit", dir = "git_dir", hidden = true })
+--
+-- function _lazygit_toggle()
+--   lazygit:toggle()
+-- end
+--
+-- vim.api.nvim_set_keymap("n", "<leader>hl", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
--- config/nvim/lua/user/plugins/which-key.lua
+++ config/nvim/lua/user/plugins/which-key.lua
@@ -1,4 +1,27 @@
-local ok, which_key = pcall(require, "which-key")
+local ok, wk = pcall(require, "which-key")
 if not ok then return end
 
-which_key.setup({})
+wk.setup()
+
+local Terminal  = require("toggleterm.terminal").Terminal
+
+local lazygit = Terminal:new({ cmd = "lazygit", dir = "git_dir", hidden = true })
+function _ToggleLazygit()
+  lazygit:toggle()
+end
+
+local float = Terminal:new({ direction = "float", hidden = true })
+function _ToggleFloat()
+  float:toggle()
+end
+
+local mappings = {
+  t = {
+    name = "Terminal",
+    t = { _ToggleFloat, "Floating Terminal" },
+    l = { _ToggleLazygit, "LazyGit Terminal" },
+  },
+}
+
+local opts = { prefix = "<leader>" }
+wk.register(mappings, opts)
