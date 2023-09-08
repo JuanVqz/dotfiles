20185421cfe9f4bbcfd95620200ecd925e8abd0b
Juan Vasquez <juan@ombulabs.com>
2023-09-08 17:12:04 -0600

    Move Telescope keybindings to which-key

--- config/nvim/lua/user/plugins/telescope.lua
+++ config/nvim/lua/user/plugins/telescope.lua
@@ -12,14 +12,14 @@ telescope.setup({
   },
 })
 
-local keymap = vim.api.nvim_set_keymap
-
-local opts = { noremap = true, silent = true }
-
-keymap("n", "<leader>ff", "<cmd>Telescope git_files<CR>", opts)
-keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
-keymap("n", "<leader>fc", "<cmd>Telescope colorscheme<CR>", opts)
-keymap("n", "<leader>fg", "<cmd>Telescope live_grep prompt_prefix=🔍<CR>", opts)
-keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
-keymap("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", opts)
-keymap("n", "<leader>fp", "<cmd>Telescope find_files<CR>", opts)
+-- local keymap = vim.api.nvim_set_keymap
+--
+-- local opts = { noremap = true, silent = true }
+--
+-- keymap("n", "<leader>ff", "<cmd>Telescope git_files<CR>", opts)
+-- keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
+-- keymap("n", "<leader>fc", "<cmd>Telescope colorscheme<CR>", opts)
+-- keymap("n", "<leader>fg", "<cmd>Telescope live_grep prompt_prefix=🔍<CR>", opts)
+-- keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
+-- keymap("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", opts)
+-- keymap("n", "<leader>fp", "<cmd>Telescope find_files<CR>", opts)
--- config/nvim/lua/user/plugins/which-key.lua
+++ config/nvim/lua/user/plugins/which-key.lua
@@ -21,6 +21,16 @@ local mappings = {
     t = { _ToggleFloat, "Floating Terminal" },
     l = { _ToggleLazygit, "LazyGit Terminal" },
   },
+  f = {
+    name = "Telescope",
+    f = { "<cmd>Telescope git_files<cr>", "Find git files" },
+    b = { "<cmd>Telescope buffers<cr>", "Find buffers" },
+    c = { "<cmd>Telescope colorscheme<cr>", "Find colorscheme" },
+    g = { "<cmd>Telescope live_grep prompt_prefix=🔍<cr>", "File Grep" },
+    h = { "<cmd>Telescope help_tags<cr>", "Find tags" },
+    k = { "<cmd>Telescope keymaps<cr>", "Find keymaps" },
+    p = { "<cmd>Telescope find_files<cr>", "Find files" },
+  },
 }
 
 local opts = { prefix = "<leader>" }
