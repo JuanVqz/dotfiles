787233426589c3d8b927ab952252f0b17f0c2c50
Juan Vasquez <juan@ombulabs.com>
2023-11-09 11:26:20 -0600

    Telescope sorting strategy desc (by default)
    there was an issue when soring descending a work around was order them
    ascending.

--- config/nvim/lua/user/plugins/telescope.lua
+++ config/nvim/lua/user/plugins/telescope.lua
@@ -5,7 +5,7 @@ if not ok then return end
 
 telescope.setup({
   defaults = {
-    sorting_strategy = "ascending",
+    -- sorting_strategy = "ascending",
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
