0c1b816f5d27af20d004324a31b23e4833f7825a
Juan Vasquez <juan@ombulabs.com>
2024-04-26 10:45:46 -0600

    Add rose-pine theme for warp and nvim

--- config/nvim/lua/colorscheme.lua
+++ config/nvim/lua/colorscheme.lua
@@ -1,4 +1,6 @@
-local colorscheme = "night-owl"
+-- local colorscheme = "solarized8_flat"
+-- local colorscheme = "night-owl"
+local colorscheme = "rose-pine-moon"
 
 local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not ok then return end
