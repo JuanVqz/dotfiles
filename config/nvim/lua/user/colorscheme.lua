1fe5ca1930379c5e6ad5b30a7126efbefd5db332
Juan Vasquez <juan@ombulabs.com>
2024-05-20 23:23:59 -0600

    Use night-owl colorschema by default

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,6 @@
 -- local colorscheme = "solarized8_flat"
--- local colorscheme = "night-owl"
-local colorscheme = "rose-pine-moon"
+-- local colorscheme = "rose-pine-moon"
+local colorscheme = "night-owl"
 
 local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not ok then return end
