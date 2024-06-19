5baea4071bda132012c60b5cd511295baecd799c
Juan Vasquez <juan@ombulabs.com>
2024-06-18 19:38:54 -0600

    Remove trim highlight

--- config/nvim/lua/user/plugins/trim.lua
+++ config/nvim/lua/user/plugins/trim.lua
@@ -8,6 +8,5 @@ return {
       -- [[%s/\%^\n\+//]],          -- trim first line
       -- [[%s/\(\n\n\)\n\+/\1/]],   -- replace multiple blank lines with a single line
     },
-    highlight = true,
   }
 }
