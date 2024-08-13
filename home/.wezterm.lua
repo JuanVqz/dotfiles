191ae9206df5d4754785c8fdf6c3954b79efdd47
Juan Vasquez <juan@ombulabs.com>
2024-08-12 21:11:59 -0600

    Add WezTerm theme: Night Owl (Gogh)

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -7,8 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Batman'
--- config.color_scheme = 'Night Owl (Gogh)'
+config.color_scheme = 'Night Owl (Gogh)'
 
 config.hide_tab_bar_if_only_one_tab = true
 
