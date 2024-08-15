a6d54af9db9b6a1a5fb778edf954cf1cedd77221
Juan Vasquez <juan@ombulabs.com>
2024-08-14 23:38:22 -0600

    Update wezterm config
    Add background opacity
    Disable audible bell

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,15 +1,13 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = 'Night Owl (Gogh)'
 
 config.hide_tab_bar_if_only_one_tab = true
 
--- and finally, return the configuration to wezterm
+config.window_background_opacity = 0.90
+
+config.audible_bell = "Disabled"
+
 return config
