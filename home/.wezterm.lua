f20dace72f9edfbb91b49ebd6f4643f4a5c45c41
Juan Vasquez <juan@ombulabs.com>
2024-08-17 13:44:16 -0600

    Update wezterm config

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -2,12 +2,17 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Night Owl (Gogh)'
-
-config.hide_tab_bar_if_only_one_tab = true
-
-config.window_background_opacity = 0.90
-
-config.audible_bell = "Disabled"
+config = {
+  color_scheme = 'Night Owl (Gogh)',
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.90,
+  audible_bell = "Disabled",
+  window_close_confirmation = "NeverPrompt",
+  window_decorations = "RESIZE",
+  line_height = 1.2,
+  window_padding = {
+    bottom = 0,
+  },
+}
 
 return config
