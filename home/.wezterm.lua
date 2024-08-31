f7fbbfc2f19a54e7ea8b5fc0da72392ec9f418b3
Juan Vasquez <juan@ombulabs.com>
2024-08-31 14:12:54 -0600

    Update wezterm font size and background opacity

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -5,11 +5,12 @@ local config = wezterm.config_builder()
 config = {
   color_scheme = 'Night Owl (Gogh)',
   hide_tab_bar_if_only_one_tab = true,
-  window_background_opacity = 0.90,
+  -- window_background_opacity = .95,
   audible_bell = "Disabled",
   window_close_confirmation = "NeverPrompt",
   window_decorations = "RESIZE",
   line_height = 1.2,
+  font_size = 12.5,
   window_padding = {
     bottom = 0,
   },
