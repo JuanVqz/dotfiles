b7087460205b6e01574549265d08968c5a86ca8e
Juan Vasquez <juan@ombulabs.com>
2024-09-16 09:54:27 -0600

    Add Telescope keybindings

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -1,5 +1,3 @@
--- For keymaps look at the which-key.lua file.
-
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
@@ -7,12 +5,21 @@ return {
     "cljoly/telescope-repo.nvim",
     "nvim-lua/plenary.nvim",
   },
+  keys = {
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+    { "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "Find colorscheme" },
+    { "<leader>ff", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "File Grep" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find tags" },
+    { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Find keymaps" },
+    { "<leader>fp", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fr", "<cmd>lua require'telescope'.extensions.repo.list{search_dirs = {'~/code', '~/.config'}}<cr>", desc = "Find repos" },
+  },
   config = function()
     local telescope = require("telescope")
 
     telescope.setup({})
 
-    pcall(require("telescope").load_extension, "fzf")
     telescope.load_extension("repo")
   end,
 }
