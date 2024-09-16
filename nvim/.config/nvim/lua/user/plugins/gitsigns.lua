999ff886c3606e6fe7068f17532e1d2d3e6858e7
Juan Vasquez <juan@ombulabs.com>
2024-09-16 09:54:41 -0600

    Fix gitsings indentation

--- nvim/.config/nvim/lua/user/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/user/plugins/gitsigns.lua
@@ -40,8 +40,8 @@ return {
     },
     on_attach = function(bufnr)
       local function map(mode, lhs, rhs, opts)
-          opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
-          vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+        opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
       end
 
       -- Navigation
