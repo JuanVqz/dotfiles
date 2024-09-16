a524fbfe459974c8275f4067a165d17a99f3ab0a
Juan Vasquez <juan@ombulabs.com>
2024-09-16 09:53:44 -0600

    Update which-key config

--- nvim/.config/nvim/lua/user/plugins/which-key.lua
+++ nvim/.config/nvim/lua/user/plugins/which-key.lua
@@ -1,39 +1,13 @@
-local M = {
+return {
   "folke/which-key.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps",
+    },
+  },
 }
-
-function M.config()
-  local wk = require "which-key"
-
-  wk.setup()
-
-  local mappings = {
-    f = {
-      name = "Telescope",
-      f = { "<cmd>Telescope git_files<cr>", "Find git files" },
-      b = { "<cmd>Telescope buffers<cr>", "Find buffers" },
-      c = { "<cmd>Telescope colorscheme<cr>", "Find colorscheme" },
-      g = { "<cmd>Telescope live_grep prompt_prefix=🔍<cr>", "File Grep" },
-      h = { "<cmd>Telescope help_tags<cr>", "Find tags" },
-      k = { "<cmd>Telescope keymaps<cr>", "Find keymaps" },
-      p = { "<cmd>Telescope find_files<cr>", "Find files" },
-      r = { ":lua require'telescope'.extensions.repo.list{search_dirs = {'~/code', '~/.config'}}<cr>", "Find repos" },
-    },
-    h = {
-      name = "Gitsigns",
-      p = { "<cmd>Gitsigns preview_hunk<CR>", "Preview Hunk" },
-    },
-    r = {
-      name = "Testing",
-      n = { ":TestNearest<CR>", "Test Nearest" },
-      f = { ":TestFile<CR>", "Test File" },
-      s = { ":TestSuite<CR>", "Test Suite" },
-      l = { ":TestLast<CR>", "Test Last" },
-    }
-  }
-
-  local opts = { prefix = "<leader>" }
-  wk.register(mappings, opts)
-end
-
-return M
