660a0b3ac7ec8f60960c213d2a4c12394fe42d94
Juan Vasquez <juan@ombulabs.com>
2025-02-08 11:56:47 -0600

    Remove lazy checker, add install missing

--- nvim/.config/nvim/lua/user/installer.lua
+++ nvim/.config/nvim/lua/user/installer.lua
@@ -23,7 +23,8 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  install = {
+    missing = truq,
+    colorscheme = { "habamax" }
+  }
 })
