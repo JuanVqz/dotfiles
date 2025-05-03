cd0783255f44c4e2a7650a3b25a6c0b45679a369
Juan Vasquez <juan@ombulabs.com>
2025-05-03 15:22:02 -0600

    Enable Telescope back

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -4,11 +4,11 @@ return {
     'nvim-lua/plenary.nvim',
   },
   keys = {
-    -- { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Find buffers' },
+    { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Find buffers' },
     { '<leader>fc', '<cmd>Telescope colorscheme<cr>', desc = 'Find colorscheme' },
-    { '<leader>fp', '<cmd>Telescope git_files<cr>', desc = 'Find git files' },
-    -- { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'File Grep' },
-    -- { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Find tags' },
+    { '<leader>ff', '<cmd>Telescope git_files<cr>', desc = 'Find git files' },
+    { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'File Grep' },
+    { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Find tags' },
     { '<leader>fk', '<cmd>Telescope keymaps<cr>', desc = 'Find keymaps' },
   },
 }
