65df58e583b35ef2d65af5051fd4edded5fee16a
Juan Vásquez <juan@ombulabs.com>
2026-01-26 13:19:35 -0600

    Update default colorscheme

--- config/.config/nvim/lua/config/colorscheme.lua
+++ config/.config/nvim/lua/config/colorscheme.lua
@@ -1,5 +1,8 @@
--- local colorscheme = 'solarized8_flat'
-local colorscheme = 'night-owl'
+local colorscheme = 'macvim'
+vim.opt.background = 'light'
+
+-- local colorscheme = 'night-owl'
+-- vim.opt.background = 'dark'
 
 local ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 if not ok then return end
